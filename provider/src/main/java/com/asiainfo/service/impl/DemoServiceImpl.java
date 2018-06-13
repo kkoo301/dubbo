08fc0c6b930d9480f68a00fc272d3ca4ed38322b
李楠 <187249935@qq.com>
2018-06-13 18:15:17 +0800

    添加 sharding

--- provider/src/main/java/com/asiainfo/service/impl/DemoServiceImpl.java
+++ provider/src/main/java/com/asiainfo/service/impl/DemoServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
@@ -31,11 +32,16 @@ public class DemoServiceImpl implements IDemoService {
 
         //Userinfo admin = mapper.findByUsername("test");
 
-        for(int i = 0 ; i < 12 ; i++){
-            Userinfo userinfo = new Userinfo();
-            userinfo.setUsername("user " + i);
-            mapper.insert(userinfo);
-        }
+        //for(int i = 0 ; i < 12 ; i++){
+        //    Userinfo userinfo = new Userinfo();
+        //    userinfo.setUsername("user " + i);
+        //    mapper.insert(userinfo);
+        //}
+
+        List<Long> ids = new ArrayList<>();
+        ids.add(1006840049003716609L);
+        ids.add(1006839628591849474L);
+        mapper.selectBatchIds(ids);
 
 
         return name + " say hello";
